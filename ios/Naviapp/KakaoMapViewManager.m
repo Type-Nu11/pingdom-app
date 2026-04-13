383e028f7da015d2521ce5d0bf128b340ab0582a
rmdkarhdwn <dkgkrlrnlvksgsk@gmail.com>
2026-04-13 20:06:24 +0900

    chore: remove duplicate iOS kakao bridge files

--- ios/Naviapp/KakaoMapViewManager.m
+++
@@ -1,6 +0,0 @@
-#import <React/RCTViewManager.h>
-
-@interface RCT_EXTERN_MODULE(KakaoMapViewManager, RCTViewManager)
-RCT_EXTERN_VIEW_PROPERTY(centerLat, NSNumber)
-RCT_EXTERN_VIEW_PROPERTY(centerLng, NSNumber)
-@end
