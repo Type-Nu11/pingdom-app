4460a7c1ff5c5c927c58c5be87e9a43da026f48b
rmdkarhdwn <dkgkrlrnlvksgsk@gmail.com>
2026-04-26 14:59:35 +0900

    feat: 권한 요청 실제로 불러오기

--- src/features/place/screens/MapScreen.tsx
+++ src/features/place/screens/MapScreen.tsx
@@ -9,18 +9,18 @@ import * as Location from 'expo-location';
 type Props = {
   language: Language;
 };
-useEffect(() => {
-  const requestPermission = async () => {
-    const {status} = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      console.log('위치 권한 거부')
-      return;
-    }
-    console.log('위치 권한 허용도')
-  }
-  requestPermission();
-})
 export default function MapScreen() {
+    useEffect(() => {
+    const requestPermission = async () => {
+      const {status} = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('위치 권한 거부')
+        return;
+      }
+      console.log('위치 권한 허용도')
+    }
+    requestPermission();
+  })
   return (
     <View style={styles.container}>
       <KakaoMapView style={styles.map} centerLat={37.402001} centerLng={127.108678} zoomLevel={7} />
