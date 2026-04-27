e96a81e196efbde28dab404d6814efec08b8e758
rmdkarhdwn <dkgkrlrnlvksgsk@gmail.com>
2026-04-27 12:32:50 +0900

    refactor: MapScreen에 useCurrentLocation 훅 연결

--- src/features/place/screens/MapScreen.tsx
+++ src/features/place/screens/MapScreen.tsx
@@ -1,19 +1,14 @@
 // screens/MapScreen.tsx
 import React from 'react';
-import { Language } from '../../../shared/i18n';
 import { StyleSheet, View } from 'react-native';
 import KakaoMapView from '../components/KakaoMapView';
-
-// MapScreen.tsx
-type Props = {
-  language: Language;
-};
-
+import { useCurrentLocation } from '../hooks/useCurrentLocation';
 
 export default function MapScreen() {
+  const center = useCurrentLocation();
   return (
     <View style={styles.container}>
-      <KakaoMapView style={styles.map} centerLat={37.402001} centerLng={127.108678} zoomLevel={7} />
+      <KakaoMapView style={styles.map} centerLat={center.lat} centerLng={center.lng} zoomLevel={7} />
     </View>
   );
 }
