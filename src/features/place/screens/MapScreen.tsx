d27ef59942326dbf211f14e6ced6d3afe9e1439d
rmdkarhdwn <dkgkrlrnlvksgsk@gmail.com>
2026-04-27 15:04:34 +0900

    feat: 자기 위치에 파란점이 뜨게 만듬

--- src/features/place/screens/MapScreen.tsx
+++ src/features/place/screens/MapScreen.tsx
@@ -8,7 +8,10 @@ export default function MapScreen() {
   const center = useCurrentLocation();
   return (
     <View style={styles.container}>
-      <KakaoMapCard style={styles.map} centerLat={center.lat} centerLng={center.lng} zoomLevel={7} />
+      <KakaoMapCard style={styles.map} centerLat={center.lat} centerLng={center.lng} zoomLevel={16} />
+      <View pointerEvents='none' style={styles.currentDotOuter}>
+        <View style={styles.currentDotInner}></View>
+      </View>
     </View>
   );
 }
@@ -17,4 +20,25 @@ export default function MapScreen() {
 const styles = StyleSheet.create({
   container: { flex: 1 },
   map: { flex: 1 },
+  currentDotOuter: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginLeft: -10,
+    marginTop: -10,
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    backgroundColor: 'rgba(30,136,229,0.25)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  currentDotInner: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: '#1E88E5',
+    borderWidth: 1.5,
+    borderColor: '#fff',
+  },
 });
