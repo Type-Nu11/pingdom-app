7d0b2f31174b3f148d99e93e961202c25234c3ca
rmdkarhdwn <dkgkrlrnlvksgsk@gmail.com>
2026-07-20 19:54:19 +0900

    Feat: V2 앱 시작 시 환경 설정 검증

--- src/v2/app/App.tsx
+++ src/v2/app/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 
 import HomeScreen from '../features/home/screens/HomeScreen';
+import '../shared/config/env';
 import AppProviders from './AppProviders';
 
 export default function App() {
